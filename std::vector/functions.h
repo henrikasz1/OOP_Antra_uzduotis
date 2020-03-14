2d8010ccbaf240d0b1100d9df6690c78ac7b20b0
henrikasz1 <58070388+henrikasz1@users.noreply.github.com>
2020-03-14 14:21:49 +0200

    v0.5 vektoriai

---
+++ std::vector/functions.h
@@ -0,0 +1,26 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <limits>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <exception>
+#include <chrono>
+#include <cctype> //skirtas patikrinti, ar tinkamai suvestas vardas/pavarde
+#include "struct.h"
+
+using namespace std;
+double median (vector<int> &A);
+double average (vector<int> &A);
+void output (int n, vector <data> &B);
+void generateFile(int kiekis, int nddydis, string &a);
+int displayHelper (int number);
+void readFile(vector<data> &M, string name, int &k, int nddydis);
+void distributeFile(int numOfStudents, int k, vector<data> &M);
+
+#endif// FUNCTIONS_H
