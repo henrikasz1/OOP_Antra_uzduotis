ee624ea72768c8dbf0e579fb98fe55703d334f18
henrikasz1 <58070388+henrikasz1@users.noreply.github.com>
2020-04-01 20:57:01 +0300

    Add files via upload

---
+++ strategy_1/std::deque/functions.h
@@ -0,0 +1,26 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <limits>
+#include <string>
+#include <iomanip>
+#include <deque>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <exception>
+#include <chrono>
+#include <cctype> //skirtas patikrinti, ar tinkamai suvestas vardas/pavarde
+#include "struct.h"
+
+using namespace std;
+double median (deque<int> &A);
+double average (deque<int> &A);
+void output (int n, deque <data> &B);
+void generateFile(int kiekis, int nddydis, string &a);
+int displayHelper (int number);
+void readFile(deque<data> &M, string name, int &k, int nddydis);
+void distributeFile(int numOfStudents, int k, deque<data> &M, deque<data> &protingi, deque<data> &kvaili);
+
+#endif// FUNCTIONS_H
